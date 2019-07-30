9ebc222d8a73c492d742b555999c4d128cb6e446
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-07-30 19:09:49 -0400

    jupyter + matplotlib + networkx = visual graphs.
    Downside is needing jupyter notebook to run ipynb files.

--- fsm.py
+++ fsm.py
@@ -1,4 +1,14 @@
-import networkx as nx
-import matplotlib as mpl
+# import networkx as nx
+# import matplotlib as mpl
+# import matplotlib.pyplot
+# %matplotlib inline
+#
+#
+# fsm = nx.Graph()
+#
+# fsm.add_node("start")
+#
+# nx.draw(fsm)
+# mpl.savefig() # save as png
+# mpl.show() # display
 
-fsm = nx.Graph()
