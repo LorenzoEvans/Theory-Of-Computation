e960adfe07ef6ce373239cfb13e47de6c02d998d
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-07-30 23:52:33 -0400

    FSM done, picture(jpg) added in project root directory.

--- fsm.py
+++ fsm.py
@@ -1,40 +1,19 @@
-# import networkx as nx
-# import matplotlib as mpl
-# import matplotlib.pyplot
-# %matplotlib inline
-# fsm = nx.Graph()
-# fsm.add_node("start")
-# nx.draw(fsm)
-# mpl.savefig() # save as png
-# mpl.show() # display
-
-
-# %matplotlib inline
-# df = pd.DataFrame({'from': ['FINISH'], 'to': ['START']})
-# G=nx.from_pandas_edgelist(df, 'from', 'to')
-# nx.draw(G, with_labels=True)
-# plt.show()
-
+# ***** LEGEND *****
+# * States = {no lights blinking, left light blinking, right light blinking, both lights blinking}
+# * State Keys(respectively) = {nlb, llb, rlb, blb}
+# * Switches(appended to reciever vert to express input value) = {left-blinker, right-blinker, panic-button}
+# * Switch Keys = {L-B, R-B, P-B}
 #### Jupyter notebook cell start ####
 
 %matplotlib inline
 from jupyterthemes import jtplot
 jtplot.style(context='talk', fscale=1, spines=False, gridlines='--')
-# CSS = """
-# .output {
-#     align-items: center;
-#     border: 1px solid black;
-# }
-# """
-
-# HTML('<style>{}</style>'.format(CSS))
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import networkx as nx
-# %matplotlib inline
 df = pd.DataFrame({'from':
                    ['start (nlb)', 'start (nlb)', 'start (nlb)', '<- (L-B) -> llb', '(P-B) -> blb' ],
                    'to': ['<- (L-B) -> llb', '(R-B) -> rlb ->', '(P-B) -> blb', '(R-B) -> rlb ->', 'start (nlb)']})
