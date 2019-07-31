18b58da5eba74fcdfc90d6f2b0dee51afd3a6f93
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-07-30 21:23:42 -0400

    Added code to fsm just because.

--- fsm.py
+++ fsm.py
@@ -12,3 +12,14 @@
 # mpl.savefig() # save as png
 # mpl.show() # display
 
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt 
+import pandas as pd
+import numpy as np
+import networkx as nx
+%matplotlib inline 
+df = pd.DataFrame({'from': ['FINISH'], 'to': ['START']})
+G=nx.from_pandas_edgelist(df, 'from', 'to')
+nx.draw(G, with_labels=True)
+plt.show()
