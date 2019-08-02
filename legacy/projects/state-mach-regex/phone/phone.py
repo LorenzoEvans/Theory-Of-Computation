eae607bc8d3936a5f91f0d3cd79a5c1a35f15e21
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-08-02 17:24:56 -0400

    Working on phone #s w/ parens.

--- legacy/projects/state-mach-regex/phone/phone.py
+++ legacy/projects/state-mach-regex/phone/phone.py
@@ -10,7 +10,7 @@ while line != "exit":
     # TODO Find matches
     line_txt = line
     match = re.findall("[0-9]{3}-[0-9]{3}-[0-9]{4}|[0-9]{3}-[0-9]{7}|[0-9]{10}", line)
-
+    match2 = re.findall("\b[(0-9)]{3}-[0-9]{3}-[0-9]{4}", line)
     # print(match)
     
     # TODO If no match found, print that no number was found
